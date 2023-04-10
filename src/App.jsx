c0711d7c51c074d8ef02fcf17cff6a8fc9d643d0
mrinnnmoy <mrinmoy202000@gmail.com>
2023-04-10 10:58:42 +0530

    add-connectWallet-btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { useAddress, ConnectWallet } from "@thirdweb-dev/react";
+
 const App = () => {
+  // Use the hooks thirdweb give us.
+  const address = useAddress();
+  console.log("Address is: ", address);
+
+  // This is the case where the user hasn't connected their wallet
+  // to your web app. Let them call connectWallet.
+  if (!address) {
+    return(
+      <div className="landing">
+        <h1>Welcome to AyodhyaDAO</h1>
+        <div className="btn-hero">
+          <ConnectWallet />
+        </div>
+      </div>
+    );
+  }
+
+  // This is the case where we have the user's address
+  // which means they've connected their wallet to our site!
+
   return (
     <div className="landing">
-      <h1>Welcome to AyodhyaDAO</h1>
+      <h1>👀 wallet connected, now what!</h1>
     </div>
   );
 };
